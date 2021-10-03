07e36c13f0453499aea67ff1d34ce7a196924428
childhoodisend <morozvktv@gmail.com>
2021-10-04 00:59:29 +0300

    [*] update main.cpp

--- calculator/main.cpp
+++ calculator/main.cpp
@@ -5,4 +5,13 @@ using namespace boost::filesystem;
 
 int main () {
     create_directory( "foobar" );
+    ofstream file( "foobar/cheeze" );
+    file << "tastes good!\n";
+    file.close();
+    if (!exists( "foobar/cheeze")) {
+        std::cout << "Something is rotten in foobar\n";
+    }
+    else {
+        std::cout << "tastes good!" << std::endl;
+    }
 }
